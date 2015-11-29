f165cc458cc93ad9ef937e0ef4779dfa147d011c
satjagan <satvik.jagannath7@gmail.com>
2015-11-29 11:04:15 +0530

    Arrow Functions - v1 ready

--- vote.js
+++ vote.js
@@ -2,16 +2,6 @@
 		var name = document.getElementById('name');
 		var age = document.getElementById('age');
 		var result = document.getElementById('result');
-		var votingRight = ageval => ageval > 18 ? true : false;
-		var vr = votingRight(age);
-		if(vr)
-		{
-			console.log("inside if "+vr);
-			result.innerHTML = name.value +"\t is Eligible To Vote";
-		}
-		else
-		{
-			console.log("inside else "+vr);
-			result.innerHTML = name.value +"\t is Not Eligible To Vote";
-		}	
+		var votingRight = ageval => ageval > 18 ? name.value +" is Eligible To Vote" : name.value +" is Not Eligible To Vote";
+		result.innerHTML = votingRight(age.value);	
 	}
